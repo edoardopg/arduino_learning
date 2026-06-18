62854822f6acab4c2d800d61f2706604563ced14
Edoardo Paba Gaddi <146628956+triuxx49@users.noreply.github.com>
2026-06-18 20:42:29 +0200

    feat: apagado automático intermitentes tras 30 segundos de inactividad

--- 04-software_intermitentes_bici/receptor-luces_intermitentes_bici/receptor-luces_intermitentes_bici.ino
+++ 04-software_intermitentes_bici/receptor-luces_intermitentes_bici/receptor-luces_intermitentes_bici.ino
@@ -4,6 +4,7 @@
 #define NUM_LEDS 4
 #define DATA_PIN_IZQ 6
 #define DATA_PIN_DER 5
+#define TIMEOUT_MS 30000  // 30 segundos
 
 CRGB leds_izq[NUM_LEDS];
 CRGB leds_der[NUM_LEDS];
@@ -12,6 +13,17 @@ RCSwitch mySwitch = RCSwitch();
 bool izquierda = false;
 bool derecha = false;
 unsigned long ultimaRecepcion = 0;
+unsigned long ultimaActivacion = 0;
+
+void apagarTodo() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds_izq[i] = CRGB::Black;
+    leds_der[i] = CRGB::Black;
+  }
+  FastLED.show();
+  izquierda = false;
+  derecha = false;
+}
 
 void efectoCascada(CRGB* leds) {
   for(int i = 0; i < NUM_LEDS; i++) {
@@ -20,9 +32,7 @@ void efectoCascada(CRGB* leds) {
     delay(120);
   }
   delay(200);
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
-  }
+  for(int i = 0; i < NUM_LEDS; i++) leds[i] = CRGB::Black;
   FastLED.show();
   delay(300);
 }
@@ -34,6 +44,11 @@ void setup() {
 }
 
 void loop() {
+  // Apagado automático por timeout
+  if ((izquierda || derecha) && (millis() - ultimaActivacion > TIMEOUT_MS)) {
+    apagarTodo();
+  }
+
   if (mySwitch.available()) {
     if (millis() - ultimaRecepcion > 1500) {
       long valor = mySwitch.getReceivedValue();
@@ -42,21 +57,20 @@ void loop() {
         derecha = false;
         for(int i = 0; i < NUM_LEDS; i++) leds_der[i] = CRGB::Black;
         FastLED.show();
+        if (izquierda) ultimaActivacion = millis();
       }
       if (valor == 2222) {
         derecha = !derecha;
         izquierda = false;
         for(int i = 0; i < NUM_LEDS; i++) leds_izq[i] = CRGB::Black;
         FastLED.show();
+        if (derecha) ultimaActivacion = millis();
       }
       ultimaRecepcion = millis();
     }
     mySwitch.resetAvailable();
   }
-  if (izquierda) {
-    efectoCascada(leds_izq);
-  }
-  if (derecha) {
-    efectoCascada(leds_der);
-  }
+
+  if (izquierda) efectoCascada(leds_izq);
+  if (derecha) efectoCascada(leds_der);
 }
